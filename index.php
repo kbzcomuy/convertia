b3f81f28e7862b17224a23e36e5d51c66029af9d
kbz <kbz@kbz.com.uy>
2021-11-03 13:13:20 -0300

    index

--- index.php
+++ index.php
@@ -31,49 +31,6 @@
 	<!-- contact  -->
 <?php include("_contact.php");?>
 
-	<?php /*
-	<section class="container-fluid full-page kbz-test custom-padd">
-		<div class="row row-fluid">
-			<div class="col colkbz">
-				<div>01 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>02 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>03 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>04 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>05 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>06 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>07 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>08 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>09 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>10 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>11 of 12</div>
-			</div>
-			<div class="col colkbz">
-				<div>12 of 12</div>
-			</div>
-		</div>
-	</section>
-	*/ ?>
-
 	<!-- footer -->
 <?php include("_footer.php");?>
 	<!-- scripts -->
