efae51a3148e9d81aa7d3e8e58dca47c7b75d295
kbz <kbz@kbz.com.uy>
2021-11-03 13:19:01 -0300

    BCKP WP

---
+++ WP-NOTES.php
@@ -0,0 +1,113 @@
+<!-- @2x -->
+<div class="img-fluid imgBannerBackground">
+	<img src="assets/img/Enmascarar.png" srcset="assets/img/Enmascarar2x.png" alt="Banner Customer Experience"/>
+</div>
+
+	   background-image: url(../img/ic-microsite-sales-banner@2x.png);
+
+
+<!-- _WORDPRESS anotaciones: -->
+
+1 + 10 + 1
+primer estructura planteada por nacho para SMART & tecnología
+
+<!-- XXXXXX -->
+<section class="XXXXXX">
+	<div class="container-fluid full-page">
+		<div class="row">
+			<div class="col-1 no-padding"></div>
+			<div class="col-10">
+				<div class="row">
+					<div class="col-1 no-padding">1</div>
+					<div class="col-1 no-padding">2</div>
+					<div class="col-1 no-padding">3</div>
+					<div class="col-1 no-padding">4</div>
+					<div class="col-1 no-padding">5</div>
+					<div class="col-1 no-padding">6</div>
+					<div class="col-1 no-padding">7</div>
+					<div class="col-1 no-padding">8</div>
+					<div class="col-1 no-padding">9</div>
+					<div class="col-1 no-padding">10</div>
+					<div class="col-1 no-padding">11</div>
+					<div class="col-1 no-padding">12</div>
+				</div>
+				<div class="row">
+					<div class="col-12 col-md-6 no-padding">6 IZQ / 12 IZQ MOBILE</div>
+					<div class="col-12 col-md-6 no-padding">6 DER / 12 DER MOBILE</div>
+				</div>
+			</div>
+			<div class="col-1 no-padding"></div>
+		</div>
+	</div>
+</section>
+<!-- NEW XXXXXX -->
+
+
+solución a 2 columnas
+
+<!-- success-stories -->
+<section class="success-stories">
+	<div class="container-fluid full-page">
+		<div class="row align-items-center">
+			<div class="col-12 col-md-4 offset-md-1 no-padding ss-left">
+				<div class="col-10 offset-1 col-md-12 offset-md-0 no-padding">
+					<h4>Casos de éxito</h4>
+					<button class="bg-blue d-none d-md-block bg-green">Conoce más</button>
+				</div>
+			</div>
+			<div class="col-12 col-md-4 offset-md-2 no-padding ss-right">
+				<div class="col-10 offset-1 col-md-12 offset-md-0 no-padding">
+					<h4>Casos de éxito</h4>
+					<button class="bg-blue d-none d-md-block bg-green">Conoce más</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- end success-stories -->
+
+<style>
+section.success-stories {
+	/*background-color: #59E2AD;*/
+	background: linear-gradient(90deg, rgba(20, 246, 187, 1) 0%, rgba(20, 246, 187, 1) 50%, rgba(255, 255, 255, 1) 50%, rgba(255, 255, 255, 1) 100%);
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer -->
+<div class="footers">
+<div class="container-fluid footer-down">
+<div class="row w-100">
+<div class="col-12 no-padding">
+<div class="copyright">© 2021 Convertia - Intelligent Customer Acquisition. <span>All Rights Reserved.</span></div>
+<div class="policy">Políticas de Privacidad: <a href="">España</a> <a href="">México</a></div>
+</div>
+</div>
+</div>
+</div>
+
+<section class="footer">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-1 no-padding"></div>
+			<div class="col-10">
+				<div class="row align-items-center">
+					<div class="col-12 col-md-6 no-padding copyright">© 2021 Convertia - Intelligent Customer Acquisition. <span>All Rights Reserved.</span></div>
+					<div class="col-12 col-md-6 no-padding policy">Políticas de Privacidad: <a href="">España</a> <a href="">México</a></div>
+				</div>
+			</div>
+			<div class="col-1 no-padding"></div>
+		</div>
+	</div>
+</section>
+
+<!-- end footer -->
